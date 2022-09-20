ed67163af61a97dd095d2a0c9edb003f169e3106
danizq303 <danizq303@alu.centroafuera.es>
2022-09-20 14:18:53 +0200

    20/09/22 14:18

--- Acceso a Datos/Clases/src/Colecciones/Clase03.java
+++ Acceso a Datos/Clases/src/Colecciones/Clase03.java
@@ -1,16 +1,30 @@
 package Colecciones;
 
-import java.util.HashSet;
+import java.util.*;
 
 public class Clase03 {
     public static void main(String[] args) {
-        HashSet<Integer> hashSet = new HashSet<>();
+        ArrayList<Integer> al = new ArrayList<>();
+
+        al.add(1);
+        al.add(4);
+        al.add(5);
+
+        HashSet<Integer> hashSet = new HashSet<>(al);
 
         hashSet.add(1);
         hashSet.add(2);
         hashSet.add(3);
         hashSet.add(1);
 
-        System.out.println(hashSet);
+        imprimirCollecion(hashSet);
+    }
+
+    public static void imprimirCollecion(Collection<Integer> array) {
+        Iterator<Integer> lit = array.iterator();
+
+        do {
+            System.out.println(lit.next());
+        } while (lit.hasNext());
     }
 }
