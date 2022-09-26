8fdcf64f863cac8dbde215884a1baf876a7d3e0c
danizq303 <danizq303@alu.centroafuera.es>
2022-09-26 14:19:35 +0200

    26/09/22 14:19

---
+++ Acceso a Datos/Clases/src/Colecciones/Persona2.java
@@ -0,0 +1,24 @@
+package Colecciones;
+
+public class Persona2 implements Comparable <Persona2>{
+    private String nombre;
+    private int edad;
+
+    public Persona2(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Persona2 o) {
+        return 0;
+    }
+}
