72d7cfbe59924fb839e93c98116b182be5321049
danizq303 <danizq303@alu.centroafuera.es>
2022-10-10 14:16:01 +0200

    10/10/22 14:16

---
+++ Acceso a Datos/Clases/src/Colecciones/Clase09.java
@@ -0,0 +1,27 @@
+package Colecciones;
+
+import java.io.*;
+
+public class Clase09 {
+    public static void main(String[] args) throws IOException {
+        File f = new File("C:\\prueba\\fichero.txt");
+
+        // System.out.println(f.getName());
+
+        BufferedWriter bW = new BufferedWriter(new FileWriter(f));
+
+        bW.append('g');
+        bW.write('x');
+        bW.append("Hola");
+
+        bW.close();
+
+        BufferedReader bR = new BufferedReader(new FileReader(f));
+
+        String str;
+        while ((str = bR.readLine()) != null)
+            System.out.println(str);
+
+        bW.close();
+    }
+}
