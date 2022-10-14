b9cfbe8ac8fffa56041abaedde14ea05b740a413
danizq303 <danizq303@alu.centroafuera.es>
2022-10-14 09:26:54 +0200

    14/10/22 09:26

--- Acceso a Datos/Clases/src/Colecciones/Clase10.java
+++ Acceso a Datos/Clases/src/Colecciones/Clase10.java
@@ -1,7 +1,27 @@
 package Colecciones;
 
-public class Clase10 {
-    public static void main(String[] args) {
+import java.io.*;
 
+public class Clase10 {
+    public static void main(String[] args) throws IOException {
+        File f = new File("C:\\prueba\\fichero.bin");
+
+        FileOutputStream fileOutputStream = new FileOutputStream(f,false);
+
+        fileOutputStream.write('c');
+        fileOutputStream.write('x');
+        fileOutputStream.write(1);
+        fileOutputStream.write(2);
+
+        fileOutputStream.close();
+
+
+        FileInputStream fileInputStream = new FileInputStream(f);
+
+        int car;
+        while ((car = fileInputStream.read()) != -1)
+            System.out.println((char)car);
+
+        fileInputStream.close();
     }
 }
---
+++ Acceso a Datos/Clases/src/Colecciones/Clase11.java
@@ -0,0 +1,23 @@
+package Colecciones;
+
+import java.io.*;
+
+public class Clase11 {
+    public static void main(String[] args) throws IOException {
+        File f = new File("C:\\prueba\\fichero.bin");
+
+        DataOutputStream dataOutputStream = new DataOutputStream(new FileOutputStream(f));
+
+        dataOutputStream.writeUTF("Hola");
+
+        DataInputStream dataInputStream = new DataInputStream(new FileInputStream(f));
+
+        try {
+            System.out.println(dataInputStream.readUTF());
+        } catch (EOFException e) {
+            System.out.println("Fin de fichero");
+        }
+
+
+    }
+}
