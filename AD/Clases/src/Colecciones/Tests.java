164f01f4216e6c399cfd02089c2a4e4b4ce63efd
danizq303 <danizq303@alu.centroafuera.es>
2022-10-26 11:17:35 +0200

    26/10/22 11:17

--- AD/Clases/src/Colecciones/Tests.java
+++ AD/Clases/src/Colecciones/Tests.java
@@ -4,6 +4,17 @@ import java.util.Iterator;
 
 public class Tests {
     public static void main(String[] args) {
+        //Crear un array de 5 string, recorrelo con un for each y muestra por pantalla el contenido.
 
+        String[] array = new String[5];
+        array[0] = "Hola";
+        array[1] = "que";
+        array[2] = "tal";
+        array[3] = "estas";
+        array[4] = "hoy";
+
+        for (String s : array) {
+            System.out.println(s);
+        }
     }
 }
