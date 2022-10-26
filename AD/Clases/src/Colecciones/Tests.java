fbce2afbc701890d9d44b23c00bc87a67e4ac85e
daniizqb <danizquiboni@gmail.com>
2022-10-26 12:04:04 +0200

    Update Tests.java

--- AD/Clases/src/Colecciones/Tests.java
+++ AD/Clases/src/Colecciones/Tests.java
@@ -1,7 +1,5 @@
 package Colecciones;
 
-import java.util.Iterator;
-
 public class Tests {
     public static void main(String[] args) {
         //Crear un array de 5 string, recorrelo con un for each y muestra por pantalla el contenido.
