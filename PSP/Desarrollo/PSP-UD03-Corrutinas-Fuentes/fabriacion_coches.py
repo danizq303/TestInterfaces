1c2a3d69616994f426eb2674462d125f6e7b1709
danizq303 <danizq303@alu.centroafuera.es>
2022-10-28 12:55:51 +0200

    28/10/22 12:55

--- PSP/Desarrollo/PSP-UD03-Corrutinas-Fuentes/fabriacion_coches.py
+++ PSP/Desarrollo/PSP-UD03-Corrutinas-Fuentes/fabriacion_coches.py
@@ -1,20 +1,3 @@
-
-#     Tarea que simula la fabricación de un juego de ruedas. Se trata de un bucle infinito en el que espera un tiempo
-#     aleatorio entre 0 y 1 segundo y envía "ruedas" a la cola entrada_fabrica
-#     Tarea que simula la fabricación de chasis. Es un bucle infinito que espera un tiempo aleatorio entre 0 y 1 segundo
-#     y envía "chasis" a la cola entrada_fabrica
-#     Tarea que simula la fabricación de motor. Es un bucle infinito que espera un tiempo aleatorio entre 0 y 1 segundo
-#     y envía "motor" a la cola de entrada de la fábrica
-#     Tarea que simula la fábrica. Es un bucle infinito que va leyendo de la cola entrada_fabrica y va almacenando en
-#     variables internas el número de juegos de ruedas, el número de chasis y el número de motores. Tras leer de la cola
-#     , puede ocurrir que pueda completar un coche (si hay al menos un chasis, un juego de ruedas y un motor). Si es así
-#     , espera un tiempo aleatorio entre 0 y 1 segundo, imprime en el log "Coche fabricado" y pone "coche" en la cola
-#     salida_fabrica. Por supuesto, la fabricación de un coche implica reducir los contadores de ruedas, chasis y motor,
-#     Tarea que simula un concesionario
-#     La corrutina main que crea las otras tareas, espera 30 segundos y cancela las que ha creado. Es un bucle infinito
-#     que lee de la cola salida_fabrica e imprime "Coche en concesionario". No es preciso que espere tiempo
-
-
 import asyncio
 import logging
 import random
