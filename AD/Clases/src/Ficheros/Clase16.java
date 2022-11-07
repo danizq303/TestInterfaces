44f10b98539fcb8479c31f2ef449e7d733559db6
danizq303 <danizq303@alu.centroafuera.es>
2022-11-07 14:12:14 +0100

    07/11/22 14:12

---
+++ AD/Clases/src/Ficheros/Clase16.java
@@ -0,0 +1,48 @@
+package Ficheros;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+public class Clase16 {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        File f = new File("C:\\prueba\\fichEmpleados.xml");
+
+        Element element = null;
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        Document document = builder.parse(f);
+        NodeList empleados = document.getElementsByTagName("empleado");
+
+        System.out.println(empleados.getLength());
+
+        /*for (int i = 0; i < empleados.getLength(); i++) {
+            Node empleado = empleados.item(i);
+            NodeList datosEmpleado = empleado.getChildNodes();
+
+            for (int j = 0; j < datosEmpleado.getLength(); j++) {
+                Node dato = datosEmpleado.item(j);
+                if (dato.getNodeType() == Node.ELEMENT_NODE) {
+                    System.out.println(dato.getNodeName() + ": " + dato.getTextContent());
+                }
+            }
+        }*/
+
+        for (int i = 0; i < empleados.getLength(); i++) {
+            Node empleado = empleados.item(i);
+            element = (Element) empleado;
+            System.out.println(element.getElementsByTagName("id").item(i).getTextContent());
+            System.out.println(element.getElementsByTagName("apellido").item(i).getTextContent());
+        }
+    }
+}
---
+++ AD/Clases/src/Ficheros/Clase17.java
@@ -0,0 +1,52 @@
+package Ficheros;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+import org.xml.sax.helpers.XMLReaderFactory;
+
+import java.io.IOException;
+
+public class Clase17 {
+    public static void main(String[] args) throws SAXException, IOException {
+        XMLReader reader = XMLReaderFactory.createXMLReader();
+
+        MiDefaultHandler handler = new MiDefaultHandler();
+
+        reader.setContentHandler(handler);
+
+        InputSource fileXML = new InputSource("C:\\prueba\\fichEmpleados.xml");
+
+        reader.parse(fileXML);
+    }
+}
+
+class MiDefaultHandler extends DefaultHandler {
+    MiDefaultHandler() {
+        super();
+    }
+
+    public void startDocument() {
+        System.out.println("Comienzo del documento XML");
+    }
+
+    public void endDocument() {
+        System.out.println("Fin del documento XML");
+    }
+
+    public void startElement(String uri, String nombre, String nombreC, Attributes atts) {
+        System.out.printf("\tComienzo elemento: %s %n", nombre);
+    }
+
+    public void endElement(String uri, String nombre, String nombreC) {
+        System.out.printf("\tFin elemento: %s %n", nombre);
+    }
+
+    public void characters(char[] ch, int inicio, int longitud) {
+        String car = new String(ch, inicio, longitud);
+        System.out.println("\t\t" + car);
+    }
+}
+
